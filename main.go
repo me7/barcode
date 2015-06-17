39949b2f32f5cf70fef03bce919332026ac6dfa6
kittys <gampolt@gmail.com>
2015-06-17 18:06:28 +0700

    initial commit

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"image/jpeg"
+	"os"
+
+	"gopkg.in/bieber/barcode.v0"
+)
+
+func main() {
+	fin, _ := os.Open("card12.jpg")
+	defer fin.Close()
+	src, _ := jpeg.Decode(fin)
+
+	img := barcode.NewImage(src)
+	scanner := barcode.NewScanner().
+		SetEnabledAll(true)
+
+	symbols, _ := scanner.ScanImage(img)
+	for _, s := range symbols {
+		fmt.Println(s.Type.Name(), s.Data, s.Quality, s.Boundary)
+	}
+}
